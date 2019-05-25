018163d2deb115ad2931b7a3a82629edb28822d0
Ricardo0623 <42210980+Ricardo0623@users.noreply.github.com>
2019-05-25 19:08:00 -0300

    EdaMerkleBlock

---
+++ EDAcoin/EDAMerkleBlock.cpp
@@ -0,0 +1,2 @@
+#include "EDAMerkleBlock.h"
+
--- EDAcoin/EDAMerkleBlock.h
+++ EDAcoin/EDAMerkleBlock.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include <stdlib.h>
+#include <string>
+#include <list>
+#include <iostream>
+using namespace std;
+
+class EDAMerkleBlock {
+public:
+	EDAMerkleBlock();
+	~EDAMerkleBlock();
+
+private:
+	unsigned int cantTransaction;
+	list<string> transactionID;
+	//list<Transaction> transactions;
+	list<unsigned int> merklePathLength;
+	//necesito una lista de merkle paths. 
+	//que seria una lista de arreglos de strings (ID)
+	// o de unsigned long pasandolo por generateID()
+	list<json> merklePath; 
+	string blockID;
+
+};
